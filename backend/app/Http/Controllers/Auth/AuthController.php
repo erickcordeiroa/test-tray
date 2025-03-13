376afba888b539cc3625f3aefd29cc3d39b8b4cf
Erick Cordeiro <erickcordeiroa@gmail.com>
2025-03-13 09:19:48 -0300

    Feat: create factory and seeder

--- backend/app/Http/Controllers/Auth/AuthController.php
+++ backend/app/Http/Controllers/Auth/AuthController.php
@@ -3,10 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use App\Services\AuthService;
 use Exception;
-use Google\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -24,11 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
+            'google_token' => Str::random(120),
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'document' => fake()->unique()->numerify('###########'),
+            'birthdate' => fake()->date(),
         ];
     }
 
